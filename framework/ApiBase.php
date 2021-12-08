625e0cf8165b4fc8a577cb9ad74daa082d1e802d
张子非 <38915715+ZhangZiFei@users.noreply.github.com>
2021-12-08 11:00:44 +0800

    Update ApiBase.php

--- framework/ApiBase.php
+++ framework/ApiBase.php
@@ -2,19 +2,96 @@
 header('application/json;charset=utf-8');
 include __DIR__ . '/Base.php';
 
-// new class
-// {
-//     public function __construct()
-//     {
-//         ob_start();
-//     }
-
-//     public function __destruct()
-//     {
-//         ob_end_flush();
-//     }
-// };
-
 class ApiBase
 {
+    /**
+     * @var ApiBase
+     */
+    public static $api;
+
+    /**
+     * 接口返回的数据
+     */
+    public static $ret = [];
+
+    /**
+     * 接口异常
+     * 
+     * `eCode`通常情况下设置为大于等于0的数字,小于0的数字仅被ApiBase.php使用
+     * 
+     * > eCode = -1 //set_error_handler error
+     * >
+     * > eCode = -2 //set_exception_handler error
+     * >
+     * > eCode = -3 //php error
+     * 
+     * eg:
+     * 
+     * ```php
+     * $e = new Exception()
+     * ApiBase::Error($e->getCode(), get_class($e), $e->getMessage());
+     * ```
+     * 
+     * @param integer $eCode 异常代码
+     * @param string $eType 异常类型
+     * @param string $eMsg 异常信息
+     * @return void
+     */
+    public static function Error(int $eCode, string $eType, string $eMsg)
+    {
+        self::$ret = [
+            'eCode' => $eCode,
+            'eType' => $eType,
+            'eMsg' => $eMsg
+        ];
+        die();
+    }
+
+    /**
+     * 接口异常
+     *
+     * @param Exception $e
+     * @return void
+     */
+    public static function Exception(Exception $e)
+    {
+        self::Error($e->getCode(), get_class($e), $e->getMessage());
+    }
+
+    //------------------------------------------------
+
+    public function __destruct()
+    {
+        echo json_encode(self::$ret);
+    }
 }
+
+ApiBase::$api = new ApiBase();
+
+//自定義異常處理
+set_exception_handler(function ($e) {
+    try {
+        http_response_code(530);
+        $log = new Tool\Log('exception');
+        $log->Error($e->__toString());
+        ApiBase::Error($e->getCode(), get_class($e), $e->getMessage());
+    } catch (\Throwable $th) {
+        ApiBase::Error(-2, 'set_exception_handler', $th->getMessage());
+    }
+});
+set_error_handler(function (int $errno, string $errstr, string $errfile, int $errline, array $errcontext = []) {
+    try {
+        http_response_code(530);
+        $log = new Tool\Log('error');
+        $log->Error($errstr, [
+            '错误级别' => $errno,
+            '错误信息' => $errstr,
+            '错误文件' => $errfile,
+            '错误行号' => $errline,
+            'errcontext' => $errcontext
+        ]);
+        ApiBase::Error(-3, 'php error', $errstr);
+    } catch (\Throwable $th) {
+        ApiBase::Error(-1, 'set_error_handler', $th->getMessage());
+    }
+});
